589849890f835fcfdcd78cbda1b7fc516d1f3b25
wangbaohui <13802885224@139.com>
2022-01-24 11:23:42 +0800

    tabbar

---
+++ LearnVuejs7/01-Router/tabberlearn/tabbar/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TabBar>
+      <TabBarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <div slot="item-text">首页</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
+        <div slot="item-text">分类</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <div slot="item-text">我的</div>
+      </TabBarItem>
+    </TabBar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar";
+import TabBarItem from "./components/tabbar/TabBarItem";
+
+export default {
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+
+
+</style>
---
+++ LearnVuejs7/01-Router/tabberlearn/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+<!--    <div>-->
+<!--      <img src="../../assets/img/tabbar/home.svg" alt="">-->
+<!--    </div>-->
+
+<!--    主页-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem"
+}
+</script>
+
+<style scoped>
+
+.tab-bar-item {
+  height: 49px;
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 2px;
+  /*去除图片下部多出来的3个像素*/
+  vertical-align: middle;
+}
+</style>
