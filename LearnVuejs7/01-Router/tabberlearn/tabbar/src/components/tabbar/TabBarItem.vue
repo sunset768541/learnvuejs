cc1b5e231d56ca648fee1b4f04ac8c8870c5638a
wangbaohui <13802885224@139.com>
2022-01-24 11:37:59 +0800

    tabbar-插槽

--- LearnVuejs7/01-Router/tabberlearn/tabbar/src/components/tabbar/TabBarItem.vue
+++ LearnVuejs7/01-Router/tabberlearn/tabbar/src/components/tabbar/TabBarItem.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="tab-bar-item">
-    <slot name="item-icon"></slot>
-    <slot name="item-text"></slot>
-<!--    <div>-->
-<!--      <img src="../../assets/img/tabbar/home.svg" alt="">-->
-<!--    </div>-->
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-icon-active"></slot>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
 
-<!--    主页-->
+    <!--    插槽这样被替换，不会有active 类加到替换过来的标签上  推荐上面的写法用div包装-->
+    <!--    <slot  :class="{active:isActive}" name="item-text"></slot>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: "TabBarItem"
+  name: "TabBarItem",
+  data() {
+    return {
+      isActive: true
+    }
+  }
 }
 </script>
 
@@ -23,6 +29,10 @@ export default {
   flex: 1;
   text-align: center;
   font-size: 14px;
+}
+
+.active {
+  color: pink;
 }
 
 .tab-bar-item img {
