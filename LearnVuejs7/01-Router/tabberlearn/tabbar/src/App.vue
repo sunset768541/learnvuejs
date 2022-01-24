f71105393fafc15eb2f200582de4e67843e2c535
wangbaohui <13802885224@139.com>
2022-01-24 14:12:30 +0800

    路径别名

--- LearnVuejs7/01-Router/tabberlearn/tabbar/src/App.vue
+++ LearnVuejs7/01-Router/tabberlearn/tabbar/src/App.vue
@@ -3,23 +3,23 @@
     <router-view></router-view>
     <TabBar>
       <TabBarItem link="/home" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </TabBarItem>
       <TabBarItem link="/category" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </TabBarItem>
       <TabBarItem link="/shopcart" activeColor="pink">
-        <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </TabBarItem>
       <TabBarItem link="/profile" activeColor="green">
-        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </TabBarItem>
     </TabBar>
@@ -27,8 +27,8 @@
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TabBarItem from "./components/tabbar/TabBarItem";
+import TabBar from "components/tabbar/TabBar";
+import TabBarItem from "components/tabbar/TabBarItem";
 
 export default {
   name: 'App',
@@ -40,7 +40,7 @@ export default {
 </script>
 
 <style>
-@import "./assets/css/base.css";
+@import "assets/css/base.css";
 
 
 </style>
