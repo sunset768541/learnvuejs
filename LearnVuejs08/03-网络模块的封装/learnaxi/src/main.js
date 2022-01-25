5d1cb31374bb1a992a855fa1c8dfa126cb6aa00d
wangbaohui <13802885224@139.com>
2022-01-25 09:20:07 +0800

    axios 基本使用

--- LearnVuejs08/03-网络模块的封装/learnaxi/src/main.js
+++ LearnVuejs08/03-网络模块的封装/learnaxi/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
@@ -7,4 +8,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+axios({url: 'http://123.207.32.32:8000/home/multidata'}).then(res => {
+  console.log('网络请求数据: ',res)
 })
