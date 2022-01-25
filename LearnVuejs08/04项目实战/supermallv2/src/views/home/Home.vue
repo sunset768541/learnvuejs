42c983cbb23bf3ff0f9c1e62558433f2cd98f6a5
wangbaohui <13802885224@139.com>
2022-01-25 16:26:00 +0800

    导航

--- LearnVuejs08/04项目实战/supermallv2/src/views/home/Home.vue
+++ LearnVuejs08/04项目实战/supermallv2/src/views/home/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <h2>
-      这是Home的界面
-    </h2>
+    <NavBar class="home-nav">
+      <h4 slot="center">主页</h4>
+    </NavBar>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #e9e9e9;
+}
 </style>
